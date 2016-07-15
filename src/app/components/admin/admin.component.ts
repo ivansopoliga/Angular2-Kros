9dedae3c408db0dd0e75b310ddb15e7fa5032e11
tomas <tomas.hlavaty14@gmail.com>
2016-07-15 14:37:36 +0200

    admin stranka

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -3,7 +3,10 @@
 @Component({
     selector: 'admin',
     templateUrl: 'app/components/admin/admin.component.html',
-    styleUrls: ['app/components/admin/admin.component.html']
+    styleUrls: ['app/components/admin/AdminLTE.css',
+                'app/components/admin/AdminLTE.min.css',
+                'app/components/admin/skins/skin-blue.css',
+                'app/components/admin/skins/skin-blue.min.css' ]
 })
 
 export class AdminComponent {
