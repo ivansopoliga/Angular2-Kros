2fc21a3974741e3b65b9c8f4c3b01ea7cfc4ccb5
postektibor <postektibor@gmail.com>
2016-07-15 15:07:52 +0200

    Users.component

--- src/app/components/admin/users.component.ts
+++ src/app/components/admin/users.component.ts
@@ -1,17 +1,20 @@
-﻿import {Component, OnInit} from '@angular/core';
+/**
+ * Created by Tibor Poštek on 15.07.2016.
+ */
+import {Component, OnInit} from '@angular/core';
 import {CORE_DIRECTIVES, FORM_DIRECTIVES, NgClass, NgIf} from '@angular/common';
 
 
 @Component({
-    selector: 'admin',
-  templateUrl: 'app/components/admin/admin.component.html',
+  selector: 'users',
+  templateUrl: 'app/components/admin/users.component.html',
   styleUrls: ['app/components/admin/css/AdminLTE.css',
     'app/components/admin/css/AdminLTE.min.css',
     'app/components/admin/css/skins/skin-blue.css',
     'app/components/admin/css/skins/skin-blue.min.css' ]
 })
 
-export class AdminComponent {
+export class UsersComponent {
 
-    private admin: boolean = true;
+  private admin: boolean = true;
 }
