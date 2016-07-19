dacd503f85ce4ebb4678dfe02d724a1d1886d429
Ondrej Pudiš <ondrejpudis@gmail.com>
2016-07-19 15:11:00 +0200

    Merge & edit admin & detail user changes

--- src/app/components/admin/offices/edit/edit.office.admin.component.ts
+++ src/app/components/admin/offices/edit/edit.office.admin.component.ts
@@ -8,17 +8,11 @@ import {Office} from './office';
 })
 
 export class EditOfficeAdminComponent {
+  public error;
+  public success;
+
   @Input() officeData:Office;
   @Output() windowClose = new EventEmitter<boolean>();
-  public sizes:Array<string> = new Array();
-
-  constructor() {
-    this.sizes['width'] = (document.documentElement.clientWidth + document.getElementById("leftMenu").clientWidth + 1).toString()+'px';
-    this.sizes['height'] = (document.getElementById("contentWindow").clientHeight + 78 + 78).toString()+'px';
-    this.sizes['modalHeight'] = (7*window.innerHeight / 8).toString()+'px';
-    this.sizes['leftMenu'] = (- document.getElementById("leftMenu").clientWidth - 1).toString()+'px';
-    this.sizes['top'] = ((window.innerHeight - 7*window.innerHeight / 8) /2).toString()+'px';
-  }
 
   closeWindow(){
     this.windowClose.emit(false);
